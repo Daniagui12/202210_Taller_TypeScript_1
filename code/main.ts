05c80fcaa3469d60961ee70837eb4c0b4bf02d1a
Daniel Aguilera <d.aguilera@uniandes.edu.co>
2022-04-13 21:00:25 -0500

    terminada primera parte

--- code/main.ts
+++ code/main.ts
@@ -13,4 +13,17 @@ function renderSeriesInTable(series: Serie[]): void {
     <td>${c.seasons}</td>`
     seriesTbody.appendChild(trElement);
   });
+  let i = average(series);
+  let trElement = document.createElement("tr");
+  trElement.innerHTML= `<td colspan="6">Seasons average: 
+  ${i}</td>`;
+  seriesTbody.appendChild(trElement);
+}
+
+function average(series: Serie[]): number {
+  let sum = 0;
+  series.forEach(c => {
+    sum += c.seasons;
+  });
+  return Math.round((sum/series.length) * 100) / 100;
 }
--- code/scripts/main.js
+++ code/scripts/main.js
@@ -7,4 +7,15 @@ function renderSeriesInTable(series) {
         trElement.innerHTML = "<td>".concat(c.id, "</td>\n    <td>").concat(c.name, "</td>\n    <td>").concat(c.channel, "</td>\n    <td>").concat(c.seasons, "</td>");
         seriesTbody.appendChild(trElement);
     });
+    var i = average(series);
+    var trElement = document.createElement("tr");
+    trElement.innerHTML = "<td colspan=\"6\">Seasons average: \n  ".concat(i, "</td>");
+    seriesTbody.appendChild(trElement);
+}
+function average(series) {
+    var sum = 0;
+    series.forEach(function (c) {
+        sum += c.seasons;
+    });
+    return Math.round((sum / series.length) * 100) / 100;
 }
