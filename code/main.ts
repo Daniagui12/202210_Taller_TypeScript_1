185e6a7b6da69dd7cfb61b3a091f37cfae93438c
Daniel Aguilera <d.aguilera@uniandes.edu.co>
2022-04-13 19:12:06 -0500

    uwu

--- code/main.ts
+++ code/main.ts
@@ -2,16 +2,15 @@ import { Serie } from "./series";
 import { series } from "./dataSeries";
 
 const seriesTbody: HTMLElement = document.getElementById('series')!;
+renderSeriesInTable(series);
 
 function renderSeriesInTable(series: Serie[]): void {
   series.forEach(c => {
     let trElement = document.createElement("tr");
     trElement.innerHTML = `<td>${c.id}</td>
-                           <td>${c.name}</td>
-                           <td>${c.channel}</td>
-                           <td>${c.seasons}</td>`;
+    <td>${c.name}</td>
+    <td>${c.channel}</td>
+    <td>${c.seasons}</td>`
     seriesTbody.appendChild(trElement);
   });
 }
-console.log(series);
-renderSeriesInTable(series);
--- code/scripts/main.js
+++ code/scripts/main.js
@@ -1,11 +1,10 @@
 import { series } from "./dataSeries";
 var seriesTbody = document.getElementById('series');
+renderSeriesInTable(series);
 function renderSeriesInTable(series) {
     series.forEach(function (c) {
         var trElement = document.createElement("tr");
-        trElement.innerHTML = "<td>".concat(c.id, "</td>\n                           <td>").concat(c.name, "</td>\n                           <td>").concat(c.channel, "</td>\n                           <td>").concat(c.seasons, "</td>");
+        trElement.innerHTML = "<td>".concat(c.id, "</td>\n    <td><a href=\"#\" id=\"").concat(c.name, "\" onclick=\"showInfo()\">").concat(c.name, "</a></td>\n    <td>").concat(c.channel, "</td>\n    <td>").concat(c.seasons, "</td>");
         seriesTbody.appendChild(trElement);
     });
 }
-console.log(series);
-renderSeriesInTable(series);
